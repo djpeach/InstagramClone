ce776d720b2e0645343d714e5f04a68ed300659b
Daniel Peach <dpeaches96@gmail.com>
2019-01-15 18:46:13 -0500

    created HomeController

---
+++ InstagramClone/Home/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  InstagramClone
+//
+//  Created by Daniel Peach on 1/15/19.
+//  Copyright © 2019 Daniel Peach. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .orange
+    }
+}
