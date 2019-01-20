0196a18198519cb3a9ebefb508a584642098efc2
Daniel Peach <dpeaches96@gmail.com>
2019-01-20 00:47:25 -0500

    Finished Lesson 29

--- InstagramClone/Home/HomeController.swift
+++ InstagramClone/Home/HomeController.swift
@@ -59,7 +59,13 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
     
     func setupNavigationItems() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "camera3").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleCamera))
         navigationItem.titleView = UIImageView(image: #imageLiteral(resourceName: "logo2"))
+    }
+    
+    @objc fileprivate func handleCamera() {
+        let cameraController = CameraController()
+        present(cameraController, animated: true, completion: nil)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
